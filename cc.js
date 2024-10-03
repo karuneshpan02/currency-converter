64320fecba5efd0f90bba88dfa4114fb4ffdb194
karuneshpan02 <socialtren.23@gmail.com>
2024-10-03 12:15:37 +0530

    Add files via upload

---
+++ cc.js
@@ -0,0 +1,19 @@
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let heigth = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + heigth){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+        };
+        });
+};
+
